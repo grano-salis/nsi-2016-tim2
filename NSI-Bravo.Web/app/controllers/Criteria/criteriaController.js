5d5850c36c54ed5cada5f23980ed31596dd88749
Azra Mahmutovic <amahmutovi3@etf.unsa.ba>
2016-11-15 01:19:27 +0100

    Criteria management

--- NSI-Bravo.Web/app/controllers/Criteria/criteriaController.js
+++ NSI-Bravo.Web/app/controllers/Criteria/criteriaController.js
@@ -132,6 +132,26 @@ app.controller('criteriaController', ['$scope', '$location', '$timeout', '$route
             $scope.GetAllMasterCriteria();
         }, 1000);
     }
+
+    $scope.tree_data = [
+     {Name:"USA",Area:9826675,Population:318212000,TimeZone:"UTC -5 to -10",
+      children:[
+        {Name:"California", Area:423970,Population:38340000,TimeZone:"Pacific Time",
+            children:[
+                {Name:"San Francisco", Area:231,Population:837442,TimeZone:"PST"},
+                {Name:"Los Angeles", Area:503,Population:3904657,TimeZone:"PST"}
+            ]
+        },
+        {Name:"Illinois", Area:57914,Population:12882135,TimeZone:"Central Time Zone",
+            children:[
+                {Name:"Chicago", Area:234,Population:2695598,TimeZone:"CST"}
+            ]
+        }
+    ]
+  },    
+  {Name:"Texas",Area:268581,Population:26448193,TimeZone:"Mountain"}
+  ];
+  
 //END DeleteCriteria
 
   
