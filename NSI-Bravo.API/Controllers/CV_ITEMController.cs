c7b9b6866ca81bcaef0a81ae7244c7bc06b824b9
ensarmuratovic <ensar_muratovic@live.com>
2016-12-06 23:33:50 +0100

    Delete viska

--- NSI-Bravo.API/Controllers/CV_ITEMController.cs
+++ NSI-Bravo.API/Controllers/CV_ITEMController.cs
@@ -198,41 +198,7 @@ namespace AngularJSAuthentication.API.Controllers
         }
 
     }
-    public class AzureStorageMultipartFormDataStreamProvider : MultipartFormDataStreamProvider
-    {
-        private readonly CloudBlobContainer _blobContainer;
-       // private readonly string[] _supportedMimeTypes = { "images/png", "images/jpeg", "images/jpg" };
-
-        public AzureStorageMultipartFormDataStreamProvider(CloudBlobContainer blobContainer) : base("AzureAttachmentsStorage")
-        {
-            _blobContainer = blobContainer;
-        }
-
-        public override Stream GetStream(HttpContent parent, HttpContentHeaders headers)
-        {
-            if (parent == null) throw new ArgumentNullException(nameof(parent));
-            if (headers == null) throw new ArgumentNullException(nameof(headers));
-            var a = parent;
-           /* if (!_supportedMimeTypes.Contains(headers.ContentType.ToString().ToLower()))
-            {
-                throw new NotSupportedException("Only jpeg and png are supported");
-            }*/
-
-            // Generate a new filename for every new blob
-            var fileName = Guid.NewGuid().ToString();
-
-            CloudBlockBlob blob = _blobContainer.GetBlockBlobReference(fileName);
-
-            if (headers.ContentType != null)
-            {
-                // Set appropriate content type for your uploaded file
-                blob.Properties.ContentType = headers.ContentType.MediaType;
-            }
-
-            this.FileData.Add(new MultipartFileData(headers, blob.Name));
-
-            return blob.OpenWrite();
-        }
-    }
+    
+    
 
 }
